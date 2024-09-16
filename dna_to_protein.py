4a9027c5559f2c380e2969acfa6ddd590ab18090
Muhammed Murshid KP <murshidcherooth@gmail.com>
2024-09-16 14:54:19 +0530

    Add files via upload

---
+++ dna_to_protein.py
@@ -0,0 +1,42 @@
+# DNA codon table
+codon_table = {
+    'ATA': 'I', 'ATC': 'I', 'ATT': 'I', 'ATG': 'M',
+    'ACA': 'T', 'ACC': 'T', 'ACG': 'T', 'ACT': 'T',
+    'AAC': 'N', 'AAT': 'N', 'AAA': 'K', 'AAG': 'K',
+    'AGC': 'S', 'AGT': 'S', 'AGA': 'R', 'AGG': 'R',
+    'CTA': 'L', 'CTC': 'L', 'CTG': 'L', 'CTT': 'L',
+    'CCA': 'P', 'CCC': 'P', 'CCG': 'P', 'CCT': 'P',
+    'CAC': 'H', 'CAT': 'H', 'CAA': 'Q', 'CAG': 'Q',
+    'CGA': 'R', 'CGC': 'R', 'CGG': 'R', 'CGT': 'R',
+    'GTA': 'V', 'GTC': 'V', 'GTG': 'V', 'GTT': 'V',
+    'GCA': 'A', 'GCC': 'A', 'GCG': 'A', 'GCT': 'A',
+    'GAC': 'D', 'GAT': 'D', 'GAA': 'E', 'GAG': 'E',
+    'GGA': 'G', 'GGC': 'G', 'GGG': 'G', 'GGT': 'G',
+    'TCA': 'S', 'TCC': 'S', 'TCG': 'S', 'TCT': 'S',
+    'TTC': 'F', 'TTT': 'F', 'TTA': 'L', 'TTG': 'L',
+    'TAC': 'Y', 'TAT': 'Y', 'TAA': '*', 'TAG': '*',
+    'TGC': 'C', 'TGT': 'C', 'TGA': '*', 'TGG': 'W',
+}
+
+
+# Function to convert DNA sequence to protein
+def dna_to_protein(dna_seq):
+    protein_seq = ""
+    # Loop through the sequence (3 nucleotides at a time)
+    for i in range(0, len(dna_seq) - 2, 3):
+        codon = dna_seq[i:i + 3]
+        # Add the corresponding amino acid to the protein sequence
+        if codon in codon_table:
+            protein_seq += codon_table[codon]
+        else:
+            protein_seq += "X"  # 'X' is for unknown codons
+
+    return protein_seq
+
+
+# Example DNA sequence
+dna_sequence = "ATGGCCATTGTAATGGGCCGCTGAAAGGGTGCCCGATAG"
+protein_sequence = dna_to_protein(dna_sequence)
+
+print("DNA Sequence: ", dna_sequence)
+print("Protein Sequence: ", protein_sequence)
